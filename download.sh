01680d405700d04ce9c12529afabd2a333669fbb
eivind-n <nilssen@exabel.com>
2021-02-28 09:57:54 +0100

    small fix

--- download.sh
+++ download.sh
@@ -2,6 +2,7 @@
 set -ef
 
 # Download Talk of Norway data
+mkdir -p ./data/raw/
 wget http://ltr.uio.no/ton/ton.data.101.tgz -O ./data/raw/ton.tgz
 tar -xzf ./data/raw/ton.tgz -C ./data/raw/
 rm ./data/raw/ton.tgz
